32bcd22b14db239cfca4f7f0e219eff76255f87b
Mark Connolly <ragu_da_sauce@me.com>
2013-06-23 14:23:34 -0400

    initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobStreet::Application.config.secret_token = 'ca1dd2db9c884f1626a6e2d063cbc0a7b92174cd7be45dffbe452441c34202e7cdee7e3762c541941d940eb0a6a0cb129a211df476af6b41ead1fdb5b929d610'
